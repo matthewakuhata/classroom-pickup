bd8775f7c436cc5a6166b03e4a3ecbb4fbd01af8
Matthew Akuhata <matthewakuhata@gmail.com>
2022-12-14 20:44:26 +1100

    Update student component add is checked state and onChange handler

--- src/Pages/Classrooms/components/Student.tsx
+++ src/Pages/Classrooms/components/Student.tsx
@@ -9,6 +9,12 @@ interface StudentProps {
 }
 
 const Student = ({ name, isActive, id, onChangeHandler }: StudentProps) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
+    onChangeHandler(event.target.checked ? 1 : -1);
+  };
   return (
       <BsPersonCircle size={20} />
       <label htmlFor={`student-checkbox-${id}`}>{name}</label>
