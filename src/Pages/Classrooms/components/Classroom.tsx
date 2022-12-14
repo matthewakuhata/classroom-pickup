bb2d5bf38005cebef7ee8208724beea4d0250819
Matthew Akuhata <matthewakuhata@gmail.com>
2022-12-14 20:48:12 +1100

    Update: add state for number of students picked up.

--- src/Pages/Classrooms/components/Classroom.tsx
+++ src/Pages/Classrooms/components/Classroom.tsx
@@ -15,10 +15,14 @@ interface ClassroomProps {
 }
 
 const Classroom = ({ students, registration }: ClassroomProps) => {
+  const [pickedUp, setPickedUp] = useState(0);
   return (
     <div className="classroom">
       <div className="classroom__heading">
         <h2>Classroom</h2>
+        <span>
+          {pickedUp} / {students.length} Picked up
+        </span>
       </div>
       <ul key={JSON.stringify(students)} className="classroom__student-list">
         {students.map((student) => (
