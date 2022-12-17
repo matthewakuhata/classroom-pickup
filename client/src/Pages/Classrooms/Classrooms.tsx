f2ba0736dc394308689dc1b79335a2766a84adda
Matthew Akuhata <matthewakuhata@gmail.com>
2022-12-17 11:02:07 +1100

    Update: use hook and cleanup static contants

--- client/src/Pages/Classrooms/Classrooms.tsx
+++ client/src/Pages/Classrooms/Classrooms.tsx
@@ -2,92 +2,41 @@ import { useRef, useState } from "react";
 import { BsSearch } from "react-icons/bs";
 
 import Classroom from "./components/Classroom";
+import { useFetchData } from "../../shared/hooks/useFetchData";
+import { ClassroomType, SeenPlates } from "./types";
 
 import "./Classrooms.scss";
 
-const CLASSROOMS = [
-  [
-    { name: "Jon Doe", id: "1", registrations: ["Q3AWT67"], classroomId: 1 },
-    {
-      name: "Miriam Fairburn ",
-      id: "2",
-      registrations: ["QS0KZH2"],
-      classroomId: 1,
-    },
-    {
-      name: "Dorthy Jack",
-      id: "3",
-      registrations: ["NV37L4O"],
-      classroomId: 1,
-    },
-    {
-      name: "Humphrey Chance",
-      id: "4",
-      registrations: ["Q3AWT67", "O10ET9K"],
-      classroomId: 1,
-    },
-    {
-      name: "Linden Wolf",
-      id: "5",
-      registrations: ["940Z895", "TKDY153"],
-      classroomId: 1,
-    },
-    {
-      name: "Doyle Bryce",
-      id: "6",
-      registrations: ["940Z895", "TKDY153"],
-      classroomId: 1,
-    },
-  ],
-  [
-    { name: "Bill Cobb", id: "7", registrations: ["2AQZ8BC"], classroomId: 2 },
-    {
-      name: "Raja Massey ",
-      id: "8",
-      registrations: ["QS0KZH2"],
-      classroomId: 2,
-    },
-    {
-      name: "Willie Mckinney",
-      id: "9",
-      registrations: ["TZX090Q", "2SAKR0N"],
-      classroomId: 2,
-    },
-    {
-      name: "Heidi Schwartz",
-      id: "10",
-      registrations: ["RZR0C5S", "Q758594"],
-      classroomId: 2,
-    },
-    {
-      name: "Emmy Mcguire",
-      id: "11",
-      registrations: ["2SAKR0N", "OETGJEN"],
-      classroomId: 2,
-    },
-  ],
-];
 const Classrooms = () => {
+  const { data: classrooms, loading } =
+    useFetchData<ClassroomType[]>("/api/v1/classroom");
+
   const [registration, setRegistration] = useState("");
+  const [seenPlates, setSeenPlates] = useState<SeenPlates>({});
   const [error, setError] = useState("");
-  const [seenRegistrations, setSeenRegistrations] = useState<{
-    [key: string]: boolean;
-  }>({});
   const regRef = useRef<HTMLInputElement>(null);
 
   const searchHandler = () => {
     if (!regRef.current) return;
 
     const value = regRef.current.value;
-    if (!value.trim()) return;
-
-    if (seenRegistrations[value]) {
-      setError(regRef.current.value);
+    if (!value.trim() || seenPlates[value]) {
+      setError(
+        value
+          ? `Registration "${value}" has already been entered. Please enter a new registration!`
+          : "Please enter a valid registration!"
+      );
       setRegistration("");
     } else {
       setRegistration(value);
-      setSeenRegistrations((prev) => ({ ...prev, [value]: true }));
+      setSeenPlates((prev) => ({ ...prev, [value]: true }));
     }
+  };
+
+  const resetHandler = () => {
+    setSeenPlates({});
+    setRegistration("");
+    setError("");
   };
 
   return (
@@ -96,29 +45,34 @@ const Classrooms = () => {
         <input
           name="registration"
           className={error ? "error" : ""}
-          onChange={() => setError("")}
+          onChange={() => registration && setRegistration("")}
+          onFocus={() => setError("")}
           ref={regRef}
           placeholder="Enter registration..."
           type="text"
         />
-        <button onClick={searchHandler}>
+        <button aria-label="search" onClick={searchHandler}>
           <BsSearch />
         </button>
       </div>
-      {error && (
-        <span className="classrooms__error-message">
-          Registration "{error}" has already been entered. Please try again!
-        </span>
+      {error && <span className="classrooms__error-message">{error}</span>}
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="classrooms-container">
+          {classrooms &&
+            classrooms.map((classroom, index) => (
+              <Classroom
+                key={`classroom-${classroom.id}`}
+                students={classroom.students}
+                registration={registration}
+              />
+            ))}
+        </div>
       )}
-      <div className="classrooms-container">
-        {CLASSROOMS.map((students, index) => (
-          <Classroom
-            key={`classroom-${index}`}
-            students={students}
-            registration={registration}
-          />
-        ))}
-      </div>
+      <button className="classrooms__reset-button" onClick={resetHandler}>
+        Reset
+      </button>
     </div>
   );
 };
